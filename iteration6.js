9ea3e312f969047b2dd64b71349fddd7b8d81b67
Clara <91693243+claraperezpolo@users.noreply.github.com>
2021-11-28 17:00:52 +0100

    Add files via upload

---
+++ iteration6.js
@@ -0,0 +1,24 @@
+//6.1
+for(var i = 0; i <= 9; i++){
+    console.log(i);
+}
+
+
+// 6.2
+for(var xi = 0; xi <= 9; xi++){
+    if(xi % 2 == 0) {
+        console.log(xi);
+    }
+}
+
+
+//6.3 
+
+for(var zi = 0; zi <= 10; zi++){
+    if(zi <= 9) {
+       console.log("Intentando dormir");
+    }else{
+        console.log("Dormido");
+    }
+}
+console.log(zi)
